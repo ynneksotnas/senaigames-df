277a014ca4e9ec8783bb0b69ee8b86d6c2d6277e
ynneksotnas <=>
2025-08-22 17:59:28 -0300

    ok

--- marketplace/models.py
+++ marketplace/models.py
@@ -23,7 +23,7 @@ class Membro(models.Model):
     email = models.CharField(max_length=50,null=False,blank=False)
     senha = models.CharField(max_length=50,null=False,blank=False)
     nome = models.CharField(max_length=80,null=False,blank=False)
-    
+    foto = models.CharField(max_length=30,null=False,blank=False)
     def __str__(self):
         return f"Nome [nome={self.nome}]"
     
